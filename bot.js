50fbd4f1359c7ca38976e7fd929c825c879bea77
Kovi524Playz <46907137+Kovi524Playz@users.noreply.github.com>
2019-01-21 22:53:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === 'Sziasztok, én kovi524 csicskája vagyok.') {
+    	message.reply('És szeretek lmczni.');
   	}
 });
 
